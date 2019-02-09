62642d8d0e9f14f8112cbc6f5d38651f8b904713
Alexander Norton <alexander.norton5@mail.dcu.ie>
2019-02-09 07:38:42 +0000

    Added a simple test

---
+++ resources/Clanbutton/Clanbutton.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Clanbutton.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Clanbutton.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fcc094a7-b195-4520-b22e-f9c9f1e3e8e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ resources/Clanbutton/Clanbutton.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Clanbutton.UnitTests
+{
+    [TestClass]
+    public class ExtensionMethodsTests
+    {
+        [TestMethod]
+        public void FakeTest_ReturnsTrue()
+        {
+            // Perform a test on a method in the Clanbutton project.
+            Assert.IsTrue(true);
+        }
+    }
+}
